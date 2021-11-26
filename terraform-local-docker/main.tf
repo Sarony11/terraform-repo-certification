9358850f299b5b0ccf28fb430a266c22e6cd7673
Saul Fernandez <sarony11@gmail.com>
2021-11-26 22:36:55 +0100

    First commit

---
+++ terraform-local-docker/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nodered_image" {
+  name = lookup(var.image, var.env)
+}
+
+resource "random_string" "random" {
+  count   = local.container_count
+  length  = 4
+  special = false
+  #upper = false￼
+}
+
+resource "docker_container" "nodered_container" {
+  count = local.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.int_port
+    external = lookup(var.ext_port[count.index], var.env)
+  }
+}
